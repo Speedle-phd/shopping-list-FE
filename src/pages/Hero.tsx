568ab3ab89605a375afdf8a12b2be1443bede537
Emanuel Eirich <shanks-@gmx.de>
2024-09-01 14:29:30 +0200

    panda

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -8,17 +8,14 @@ const Hero = () => {
          <Heading color='blue.500'>#Shopping with<br/>
          Michi</Heading>
          <Text color='gray.600' marginBlock={8}>
-            {/* Welcome to Panda's shopping reminder list.
+            Welcome to Panda's shopping reminder list.
             <br />
             Since Ono always yearns for waffles, <br />
             he programmed this app, so buttermilk is bought <br />
             on every occasion and will never be forgotten again. <br />
             <br />
             Log in to manage your collections, <br />
-            but don't forget the buttermilk. */}
-            Willkommen zu Michi's Einkaufs-Erinnerungsliste. <br />
-            Da Michi zur Zeit nicht genug von Süßkartoffeln-Pasta bekommt, muss sie eine nie abschließende Liste haben, um sicherzustellen, dass sie immer genug Zutaten hat. <br />
-            Wäre dies nicht gewährleistet, würde sie sich in eine süße Süßkartoffel verwandeln. <br />
+            but don't forget the buttermilk.
          </Text>
          <Link tabIndex={-1} to='login'>
             <Button
